adac462710a0b2d2fc32d4ba339ab58860840259
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-06-26 14:45:01 +0200

    add thanks modal with timeout to hide

--- app/javascript/components/total_click.js
+++ app/javascript/components/total_click.js
@@ -2,6 +2,21 @@ const totalClick = () => {
   const totalButton = document.querySelector('#total-link');
   if (totalButton) {
     totalButton.addEventListener('click', () => {
+      const like = parseInt(localStorage.getItem('like'), 10)
+      const dislike = parseInt(localStorage.getItem('dislike'), 10)
+      const total = parseInt(localStorage.getItem('like'), 10) + parseInt(localStorage.getItem('dislike'), 10)
+      const dislikeTd = document.querySelector('#dislike-td')
+      const likeTd = document.querySelector('#like-td')
+      const totalTd = document.querySelector('#total-td')
+      const dislikeStatTd = document.querySelector('#dislike-stats-td')
+      const likeStatTd = document.querySelector('#like-stats-td')
+      dislikeTd.innerText = dislike
+      likeTd.innerText = like
+      totalTd.innerText = total
+      const dislikeStats = Math.round((dislike / total)*100)
+      const likeStats = Math.round((like / total)*100)
+      dislikeStatTd.innerText = `${dislikeStats}%`
+      likeStatTd.innerText = `${likeStats}%`
       const totalContainer = document.querySelector('.total-container')
       totalContainer.style.display = 'block';
     })
