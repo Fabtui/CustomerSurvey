9eff61f90b6aa1175b8249326be88a9ca080d7a1
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-06-26 13:06:48 +0200

    add stats on table

--- app/javascript/components/survey_click.js
+++ app/javascript/components/survey_click.js
@@ -7,12 +7,31 @@ const surveyClick = () => {
       let likeCount = parseInt(localStorage.getItem(like), 10)
       likeCount++
       localStorage.setItem(like, (likeCount).toString())
+      displayCount();
       console.log(localStorage.getItem(like));
     }
 
     likeFaces.forEach(likeFace => {
       likeFace.addEventListener('click', (e) => { addToLocals(e.currentTarget.id) })
     });
+
+    const displayCount = () => {
+      const like = parseInt(localStorage.getItem('like'), 10)
+      const dislike = parseInt(localStorage.getItem('dislike'), 10)
+      const total = parseInt(localStorage.getItem('like'), 10) + parseInt(localStorage.getItem('dislike'), 10)
+      const dislikeTd = document.querySelector('#dislike-td')
+      const likeTd = document.querySelector('#like-td')
+      const totalTd = document.querySelector('#total-td')
+      const dislikeStatTd = document.querySelector('#dislike-stats-td')
+      const likeStatTd = document.querySelector('#like-stats-td')
+      dislikeTd.innerText = dislike
+      likeTd.innerText = like
+      totalTd.innerText = total
+      const dislikeStats = Math.round((dislike / total)*100)
+      const likeStats = Math.round((like / total)*100)
+      dislikeStatTd.innerText = `${dislikeStats}%`
+      likeStatTd.innerText = `${likeStats}%`
+    }
   }
 
 }
