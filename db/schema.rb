1e8e30118f2cc3cd1ca86c25eca537a29b9a3f59
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-06-29 18:33:53 +0200

    add middle migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_27_214434) do
+ActiveRecord::Schema.define(version: 2022_06_29_163024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2022_06_27_214434) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "tag_line"
     t.boolean "selected"
+    t.integer "middle"
     t.index ["user_id"], name: "index_days_on_user_id"
   end
 
