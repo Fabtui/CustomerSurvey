d2955899689d012a0934627a502fba5a60201d57
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-06-30 13:01:16 +0200

    remove sweet alert outside home except for log out

--- app/javascript/components/init_sweet_alert.js
+++ app/javascript/components/init_sweet_alert.js
@@ -1,12 +1,17 @@
 import swal from 'sweetalert';
 
 const initSweetalert = (selector, options = {}, callback = () => {}) => {
+  const homeContainer = document.querySelector('.home-container')
   const swalButton = document.querySelector(selector);
-  if (swalButton) { // protect other pages
+  if (homeContainer && swalButton || swalButton && selector == "#log-out-link-button") { // protect other pages
     swalButton.addEventListener('click', () => {
       swal(options).then(callback);
     });
-  }
-};
-
+  } else if (swalButton && selector != "#log-out-link-button") { // protect other pages
+    swalButton.addEventListener('click', () => {
+      const link = document.querySelector(selector.replace('-button', ''));
+      link.click();
+    });
+  };
+}
 export { initSweetalert };
