3c4f2dd1ea7009281fd1b1173746e2a09759b0dc
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-07-04 19:06:28 +0200

    add simleys to chart

--- app/javascript/components/chart.js
+++ app/javascript/components/chart.js
@@ -1,43 +1,40 @@
 import Chart from 'chart.js/auto';
-import goodImage from '../../assets/images/like.png'
-import middleImage from '../../assets/images/middle.png'
-import badImage from '../../assets/images/dislike.png'
 
 const chartJs = () => {
   const indexContainer = document.querySelector('.days-container')
   if (indexContainer) {
     const ctx = document.querySelectorAll('#myChart');
-    console.log(goodImage);
-    const img = new Image(50, 50); // width, height
-    img.src = goodImage;
     ctx.forEach(ctx => {
       const good = ctx.dataset.good
       const middle = ctx.dataset.middle
       const bad = ctx.dataset.bad
       const total = ctx.dataset.total
+      const goodPc = `${Math.round(parseInt(good, 10) * 100) / parseInt(total, 10)}%`
+      const middlePc = `${Math.round(parseInt(middle, 10) * 100) / parseInt(total, 10)}%`
+      const badPc = `${Math.round(parseInt(bad, 10) * 100) / parseInt(total, 10)}%`
       const myChart = new Chart(ctx, {
-          type: 'bar',
-          data: {
-              labels: ['', '', '', ''],
-              datasets: [{
-                  label: 'Nombre de votes',
-                  data: [good, middle, good, total],
-                  backgroundColor: [
-                      'rgba(255, 99, 132, 0.2)',
-                      'rgba(255, 206, 86, 0.2)',
-                      'rgba(75, 192, 192, 0.2)',
-                      'rgba(0, 0, 0, 0.2)'
-                  ],
-                  borderColor: [
-                      'rgba(255, 99, 132, 1)',
-                      'rgba(255, 206, 86, 1)',
-                      'rgba(75, 192, 192, 1)',
-                      'rgba(0, 0, 0, 0.8)'
-                  ],
-                  borderWidth: 1
-              }]
-          },
-        options: {
+        type: 'bar',
+        data: {
+          labels: ['', '', '', ''],
+          datasets: [{
+            label: 'Nombre de votes',
+            data: [bad, middle, good, total],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(14, 203, 67, 0.2)',
+              'rgba(0, 0, 0, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(14, 203, 67, 1)',
+              'rgba(0, 0, 0, 0.8)'
+            ],
+            borderWidth: 1
+          }]
+        },
+      options: {
         }
       });
     });
