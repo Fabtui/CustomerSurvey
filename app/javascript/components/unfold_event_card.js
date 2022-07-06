a0306df3f6f875ff1f8cc5cb343303b9e55fca2b
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-07-06 10:33:26 +0200

    add unfold/fold on event cards

--- app/javascript/components/unfold_event_card.js
+++ app/javascript/components/unfold_event_card.js
@@ -1,15 +1,20 @@
 const unfoldEventCard = () => {
   const eventContainer = document.querySelector('.events-container')
   if (eventContainer) {
-    const statItems = document.querySelectorAll('.stats-item');
-    statItems.forEach(statItem => {
-      statItem.addEventListener('click', (e) => {
+    const faIcons = document.querySelectorAll('.unfold__arrow__container > .fa');
+    faIcons.forEach(faIcon => {
+      faIcon.addEventListener('click', (e) => {
         const id = e.target.id;
-        console.log(`#container-${id}`);
         const eventContainer = document.querySelector(`#container-${id}`)
         eventContainer.classList.toggle('fold__container');
         const eventContent = document.querySelector(`#content-${id}`)
         eventContent.classList.toggle('fold__content');
+        console.log(e.target.classList);
+        if (e.target.classList.contains('fa-angle-down')) {
+          e.target.classList.replace('fa-angle-down', 'fa-angle-up')
+        } else {
+          e.target.classList.replace('fa-angle-up', 'fa-angle-down')
+        }
       })
     });
   };
